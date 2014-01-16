5394faf11c38d483b75077cee0e8e1f189bdfd51
James Rasmussen <james@delilah.local>
2014-01-16 16:25:35 -0700

    cleaned up code, downloader now checks if after 2am the next day

--- downloader.py
+++ downloader.py
@@ -7,7 +7,8 @@ import getpass
 import time
 import subprocess
 import sqlite3
-import datetime
+from datetime import datetime, date, timedelta
+from datetime import date
 from bs4 import BeautifulSoup as bs4
 from AppKit import NSWorkspace, NSScreen
 from Foundation import NSURL
@@ -17,19 +18,21 @@ from Foundation import NSURL
 This simple script grabs the daily National Geographic
 image of the day, and sets it as your desktop background
 '''
-
 def get_image_link_and_title():
     try:
-        page = urllib2.urlopen("http://photography.nationalgeographic.com/photography/photo-of-the-day/")
+        page = urllib2.urlopen(
+            "http://photography.nationalgeographic.com/photography/photo-of-the-day/")
     except urllib2.URLError:
-        #Looks like it didn't work, just return from the function and try again at the next interval
+        # Looks like it didn't work, just return from the function 
+        # and try again at the next interval
         print "there was an error opening the url"
         return None, None
     soup = bs4(page)
     try:
         link = soup.find('div', class_='download_link').find('a').attrs['href']
     except AttributeError:
-        #looks like there wasn't a download link. just grab the low-res image instead
+        #looks like there wasn't a download link. 
+        # just grab the low-res image instead
         link = soup.find('div', class_='primary_photo').find('img').attrs['src']
 
     # now, prepend http onto the url
@@ -42,7 +45,8 @@ def download_and_name_image(link, title):
     path_to_pictures = '/Users/' + getpass.getuser() + '/Pictures/'
 
     # sets the image name and file extension
-    desktop_picture_path = '{0}NatGeo - {1}.{2}'.format(path_to_pictures, title, link.split('.')[-1])
+    desktop_picture_path = '{0}NatGeo - {1}.{2}'.format(
+                            path_to_pictures, title, link.split('.')[-1])
 
     # writes the file and returns the path
     output = open(desktop_picture_path, 'w')
@@ -59,19 +63,25 @@ def set_desktop_background(desktop_picture_path):
     for screen in screens:
         ws.setDesktopImageURL_forScreen_options_error_(file_url, screen, {}, None)
 
-def should_download_new_image(next_image_time):
-    return datetime.datetime.now() >= next_image_time
+def should_download_new_image(next_check_time):
+    return datetime.now() >= next_check_time
+
+def two_am_tomorrow():
+    return (datetime.combine(date.today() + 
+            timedelta(1), datetime.min.time()) + 
+            timedelta(hours=2))
 
 def main():
     old_image = ''
-    next_image_time = datetime.datetime.now()
+    next_check_time = datetime.now()
     while True:
-        if should_download_new_image(next_image_time):
+        if should_download_new_image(next_check_time):
             link, title = get_image_link_and_title()
             if link != None and title != None:
                 desktop_picture_path = download_and_name_image(link, title)
                 set_desktop_background(desktop_picture_path)
-                next_image_time = next_image_time + datetime.timedelta(1)
+                # set the next check time to 2am the next day.
+                next_check_time = two_am_tomorrow() 
         time.sleep(30)
 
 
